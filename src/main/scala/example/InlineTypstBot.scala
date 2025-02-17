a27eee1d75622b39a62ffd40d4157950d714bec3
Armeddon <artkul204@gmail.com>
2025-02-17 12:05:29 +0300

    Initial commit

---
+++ src/main/scala/example/InlineTypstBot.scala
@@ -0,0 +1,65 @@
+package example
+
+import cats.implicits._
+
+import cats.effect._
+import cats.effect.Async
+import cats.Parallel
+
+import telegramium.bots.high._
+import telegramium.bots.high.implicits._
+import telegramium.bots.Message
+
+import java.nio.file.{Files, Paths}
+import java.util.Base64
+
+class InlineTypstBot(imgbbKey: String)(implicit
+  bot: Api[IO],
+  asyncF: Async[IO],
+  parallel: Parallel[IO]
+  ) extends LongPollBot[IO](bot) {
+
+    import cats.syntax.flatMap._
+    import cats.syntax.functor._
+    import telegramium.bots._
+
+    override def onMessage(msg: Message): IO[Unit] = {
+      msg.text match {
+        case Some(s) if s.toLowerCase.startsWith("/") =>
+          sendMessage(
+            chatId = ChatIntId(msg.chat.id),
+            text = "That's some command",
+          ).exec.void
+        case Some(_) => 
+          sendMessage(
+            chatId = ChatIntId(msg.chat.id),
+            text = "Hello, world!",
+          ).exec.void
+        case None => asyncF.unit
+      }
+    }
+
+    override def onInlineQuery(query: InlineQuery): IO[Unit] = {
+      def answer(image: Option[Image]): IO[Unit] = {
+        answerInlineQuery(
+          inlineQueryId = query.id,
+          results = image match {
+            case Some(Image(url, width, height)) => List(InlineQueryResultPhoto(
+                id = "0",
+                photoUrl = url,
+                thumbnailUrl = url,
+                photoWidth = Some(width),
+                photoHeight = Some(height),
+              ))
+            case None => Nil
+          },
+          cacheTime = Some(0),
+        ).exec.void
+      }
+
+      for {
+        result <- TypstBuilder.build(query.query, imgbbKey)
+        _ <- answer(result)
+      } yield ()
+    }
+}
