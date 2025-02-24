1d4a45aa761b5b06aa4d48e60afa5ac0e5f700b9
Armeddon <artkul204@gmail.com>
2025-02-25 00:20:55 +0300

    Another initial commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+object Dependencies {
+  lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
+  lazy val telegramium_core = "io.github.apimorphism" %% "telegramium-core" % "9.802.0"
+  lazy val telegramium_high = "io.github.apimorphism" %% "telegramium-high" % "9.802.0"
+  lazy val cats_effect = "org.typelevel" %% "cats-effect" % "3.5.7"
+}
